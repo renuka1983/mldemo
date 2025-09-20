c1d691832152363ef2ec6419e0f4246424ca92cc
renuka1983 <renu.stat@gmail.com>
2025-09-20 10:31:23 +0530

    Added add function and unit test

---
+++ test_math_utils.py
@@ -0,0 +1,7 @@
+import math_utils
+
+def test_add():
+    assert math_utils.add(2, 3) == 5
+    assert math_utils.add(-1, 1) == 0
+    assert math_utils.add(0, 0) == 0
+    assert math_utils.subtract(1, 0) == 1
