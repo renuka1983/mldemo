01d3e768d2afee7dbdca8479e8d22e85ca67f1ca
renuka1983 <renu.stat@gmail.com>
2025-09-27 07:14:38 +0530

    fixed the errors

--- train.py
+++ train.py
@@ -3,12 +3,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-df = read_csv('data.csv')
+df = pd.read_csv('data.csv')
 X = df.drop('target', axis=1)
 y = df['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-model = LogisticRegression()
+model = LogisticRegression(random_state=42)
 model.fit(X_train, y_train)
 
 print("Model trained successfully.")
